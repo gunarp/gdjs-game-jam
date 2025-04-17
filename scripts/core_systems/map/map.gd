9ef1fef4185b3cebfcc967e67e9391c03077c9e6
Sunny Hu <hu.sunnya@gmail.com>
2025-04-16 23:28:45 -0700

    initial map.gd commit, no Db name change

--- scripts/core_systems/map/map.gd
+++ scripts/core_systems/map/map.gd
@@ -2,30 +2,51 @@ extends Node
 
 class_name Map
 
-func _ready() -> void:
-  pass
-
 class Neighbor:
   var seat_id: int
   var distance: int
+  
+class Seat_Info:
+  var unit_id = -1
+  var neighbors: Array[Neighbor]
+
+var map: Array[Seat_Info] = []
+
+func _ready() -> void:
+  var seat_vals = [1, 0, 3, 2]
+  for i in range(4):
+    var temp = Neighbor.new()
+    temp.seat_id = seat_vals[i]
+    temp.distance = 1
+    var temp_seat = Seat_Info.new()
+    temp_seat.neighbors = temp
+    map.append(temp_seat)
+  
+  print(map)
+  pass
 
 func get_neighbors(seat_id: int) -> Array[Neighbor]:
-  return []
-
+  return map[seat_id].neighbors
 
 func set_seat(seat_id: int, unit_id: int) -> void:
+  map[seat_id].unit_id = unit_id
   pass
-
 
 func clear_seat(seat_id: int) -> void:
+  map[seat_id].unit_id = -1
   pass
-
 
 # return a -1 if the seat is empty
 func get_seat(seat_id: int) -> int:
-  return 0
-
+  return map[seat_id].unit_id
 
 func change_map(new_map_data: MapData) -> void:
   pass
 
+# if all of the seats are full, returns 1
+func is_seating_full() -> bool:
+  var count = 0
+  for i in map.size():
+    # if seat is not empty, increase count by 1
+    count += int(get_seat(i) != -1)
+  return count == map.size()
